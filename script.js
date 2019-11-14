dd079b4b574d09a8fd71935b3ca7c7c220711082
Aryeh (Ari) Tsetlin <57076256+ATsetlin@users.noreply.github.com>
2019-11-14 16:19:20 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let flappy = document.querySelector('#flappy');
+
+const ethan = () => {
+	flappy.setAttribute('hidden', 'display:inline !important');
+	flappy.setAttribute("hidden", "true");
+	document.getElementsByTagName('canvas')[0].setAttribute("hidden", "true");
+	// hidFile.setAttribute("hidden", "false");
+	let hidFile = document.querySelector('#containertic');
+	hidFile.style.display = "block";
+}
+
 const winningCombos = [
     [0,1,2],
     [3,4,5],
@@ -29,6 +40,7 @@ const checkForVictory = () => {
         if(allSame(sequence)) {
             victory = true;
             endGame(sequence);
+            ari();
         }
     });
     return victory;
@@ -53,3 +65,32 @@ const enableListeners = () => grid().forEach(_qEl => _qEl.addEventListener('clic
 const disableListeners = () => grid().forEach(_qEl => _qEl.removeEventListener('click', clickFn));
 
 enableListeners();
+
+
+const ari = () => {
+	let hidFile = document.querySelector('#containertic');
+	hidFile.style.display = "none";
+	let website = document.querySelector('#hidden');
+	website.style.display = "block";
+	// flappy.setAttribute('hidden', 'display:inline !important');
+	// flappy.setAttribute("hidden", "true");
+	// document.getElementsByTagName('canvas')[0].setAttribute("hidden", "true");
+	// hidFile.setAttribute("hidden", "false");
+}
+// const ethan = () => {
+// 	flappy.setAttribute('hidden', 'display:inline !important');
+// 	flappy.setAttribute("hidden", "true");
+// 	document.getElementsByTagName('canvas')[0].setAttribute("hidden", "true");
+// 	// hidFile.setAttribute("hidden", "false");
+// 	let hidFile = document.querySelector('#containertic');
+// 	hidFile.style.display = "block";
+// }
+
+
+let trump = document.querySelector('#trump');
+
+const gif = () => {
+	trump.style.display = "block";
+}
+
+document.querySelector('#gifbut').addEventListener("click", gif);
