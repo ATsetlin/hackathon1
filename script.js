c23d1842217cd19d4b8434f3e67e5f50178585c7
elibrand <57098028+elibrand@users.noreply.github.com>
2019-11-14 09:41:27 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-let test = document.querySelector('#test');
-let hidFile = document.querySelector('#hidden');
-test.onclick = function(){
+let flappy = document.querySelector('#flappy');
+
+const ethan = () => {
+	flappy.setAttribute('hidden', 'display:inline !important');
+	flappy.setAttribute("hidden", "true");
+	document.getElementsByTagName('canvas')[0].setAttribute("hidden", "true");
 	// hidFile.setAttribute("hidden", "false");
+	let hidFile = document.querySelector('#hidden');
 	hidFile.style.display = "block";
-	test.setAttribute("hidden", "true");
 }
